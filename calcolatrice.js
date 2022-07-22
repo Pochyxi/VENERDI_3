879017f954948603a10f74a86a27699228bd030d
Pochyxi <adienerlopez@gmail.com>
2022-07-22 23:31:36 +0200

    first commit

---
+++ calcolatrice.js
@@ -0,0 +1,16 @@
+
+function number (dato) {
+    document.getElementById('operazioni').value += dato;
+}
+
+function operazione() {
+    document.getElementById('operazioni').value = eval(document.getElementById('operazioni').value)
+   
+    
+
+}
+
+function cancella() {
+    document.getElementById('operazioni').value = '';
+}
+
